f6f75f86b3688ef08e20513462639b5c1d33e518
seakintruth <seakintruth@gmail.com>
2026-08-24 13:27:02 -0400

    [PrizmForge] Agent edit via proposal fd423604

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  clearMocks: true,
+  restoreMocks: true,
+  verbose: true,
 };
